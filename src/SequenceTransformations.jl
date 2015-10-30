cb3367d8f5ef92d5ece4877a90edfc8973524a96
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-10-30 08:19:25 +0000

    SequenceTransformations.jl generated files.
        license:  MIT
        authors:  Richard Mikael Slevinsky
        years:    2015
        user:
    
    Julia Version 0.4.1-pre+8 [1bb7e10*]

---
+++ src/SequenceTransformations.jl
@@ -0,0 +1,5 @@
+module SequenceTransformations
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using SequenceTransformations
+using Base.Test
+
+# write your own tests here
+@test 1 == 1
